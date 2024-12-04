097aac6febd14cb3d30404425baa0f2cea7d82a6
saintzema <emezeji98@gmail.com>
2024-12-04 18:10:15 -0500

    First Commit

---
+++ database/seeders/JobSeeder.php
@@ -0,0 +1,53 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\Employer;
+use App\Models\Job;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class JobSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tags = Tag::factory(3)->create(); // Create 3 new tags
+        $employers = Employer::factory(5)->create();
+
+        Job::factory(5)->hasAttached($tags)->create(new Sequence([
+            'featured' => false,
+            'schedule' => 'Full Time',
+        ], [
+            'featured' => true,
+            'schedule' => 'Part Time',
+        ]));
+
+        $jobTitles = [
+            'Web Developer',
+            'Data Analyst',
+            'Project Manager',
+            'Graphic Designer',
+            'Financial Specialist',
+            'Software Engineer',
+            'Product Manager',
+            'UX/UI Designer',
+            'DevOps Engineer',
+            'Marketing Specialist',
+        ];
+
+        foreach ($employers as $employer) {
+            foreach ($jobTitles as $title) {
+                Job::factory()->create([
+                    'employer_id' => $employer->id,
+                    'title' => $title, // Assign unique title
+                    'salary' => rand(50000, 120000), // Random salary
+                ])->each(function ($job) use ($tags) {
+                    $job->tags()->attach($tags->random(rand(1, 5))->pluck('id')->toArray()); // Attach random tags (1 to 3)
+                });
+            }
+        }
+    }
+}
---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,9 @@
+@php
+
+$classes = "p-4 bg-white/5 rounded-xl border border-transparent hover:border-blue-600 group transition-colors duration-500";
+
+@endphp
+
+<div {{$attributes(['class' => $classes])}}>
+{{$slot}}
+</div>
